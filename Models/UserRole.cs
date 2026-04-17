6f96199807b648379f1799e5ab0bd022e8c3e7d6
JhonnyJJRG <jhonnyjesus19@gmail.com>
2026-04-16 19:50:12 -0500

    .

--- Models/UserRole.cs
+++ Models/UserRole.cs
@@ -7,10 +7,10 @@ namespace ExamenFinalGym.Models
         public int UserId { get; set; }
         public int RoleId { get; set; }
         
-        [JsonIgnore]
+       
         public Usuario? Usuario { get; set; }
 
-        [JsonIgnore]
+        
         public Role? Role { get; set; }
     
         public DateTime AssignedAt { get; set; } = DateTime.Now;
